031236c849b5cf3e5ad3c0152dffb87bb61f06e7
Nina <tuyetnhung100@me.com>
2021-02-07 00:08:48 +0300

    add: Calculator app

---
+++ calculator-app.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float num1, num2;
+	char operation;
+	cout << "My Calculator" << endl;
+	cin >> num1 >> operation >> num2;
+	
+	switch (operation)
+	{
+	case '-': cout << num1 << operation << num2 << " = " << num1 - num2 << endl; break;
+	case '+': cout << num1 << operation << num2 << " = " << num1 + num2 << endl; break;
+	case '/': cout << num1 << operation << num2 << " = " << num1 / num2 << endl; break;
+	case '*': cout << num1 << operation << num2 << " = " << num1 * num2 << endl; break;
+	case '%':
+		  bool isNum1Int, isNum2Int;
+		  isNum1Int = ((int)num1 == num1);
+		  isNum2Int = ((int)num2 == num2);
+
+		  if (isNum1Int && isNum2Int)
+			  cout << num1 << operation << num2 << " = " << (int)num1 % (int)num2 << endl; 
+		  else 
+			  cout << "Invalid numbers entered" << endl;
+		  break;
+	default: cout << "Invalid operation" << endl;
+
+	}
+}
