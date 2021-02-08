be9d9793c4284e592bf063df6c375dd97f983120
Nina <tuyetnhung100@me.com>
2021-02-08 16:42:33 +0300

    add: Check for negative number.

--- Factorial.cpp
+++ Factorial.cpp
@@ -11,13 +11,17 @@ int main() {
 	
 	int factorial = 1;
 
-	/*for (int i = 1; i <= number; i++) {
-		factorial = factorial * i;
-	}*/
+	if (number <= 0) 
+		cout << "Cannot factorial negative numbers or zero.\n";
+	else {
+		/*for (int i = 1; i <= number; i++) {
+			factorial = factorial * i;
+		}*/
 
-	//6!=6*5*4*3*2*1=720
-	for (int i = number; i >= 1; i--) {
-		factorial *= i;
+		//6!=6*5*4*3*2*1=720
+		for (int i = number; i >= 1; i--) {
+			factorial *= i;
+		}
+		cout << number << "!=" << factorial << endl;
 	}
-	cout << number << "!=" << factorial << endl;
 }
