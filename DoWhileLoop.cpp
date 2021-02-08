064eaba9416292fd7efa8f8bc0255c0b5775c649
Nina <tuyetnhung100@me.com>
2021-02-08 15:31:21 +0300

    add: Program for PIN validation.

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Validate Pin 3 times
+	
+	int usersPin = 1234, pin, errorCounter = 0;
+
+	do {
+		cout << "PIN: ";
+		cin >> pin;
+		if (pin != usersPin)
+			errorCounter++;
+	} while (errorCounter < 3 && pin != usersPin);
+
+	if (errorCounter < 3)
+	       cout << "Loading...\n";
+	else
+		cout << "Blocked...\n";	
+
+}
