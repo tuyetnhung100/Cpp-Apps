4d8eda7597df140f97d5ac553a7de7569091a356
Nina <tuyetnhung100@me.com>
2021-02-08 16:27:39 +0300

    add: Program for calculating the factorial of a number.

---
+++ Factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//The Factorial of a number
+	//6!=1*2*3*4*5*6=720
+
+	int number;
+	cout << "Number: ";
+	cin >> number;
+	
+	int factorial = 1;
+
+	/*for (int i = 1; i <= number; i++) {
+		factorial = factorial * i;
+	}*/
+
+	//6!=6*5*4*3*2*1=720
+	for (int i = number; i >= 1; i--) {
+		factorial *= i;
+	}
+	cout << number << "!=" << factorial << endl;
+}
